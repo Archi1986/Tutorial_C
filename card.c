de9fe4d7fb5d9ee0f048ef63f494a390a955c65c
Yung-Chi_Hsu <Syungchi@gmailcom>
2017-11-16 00:55:52 +0800

    Update condition about print 3~6 JQK

--- card.c
+++ card.c
@@ -17,6 +17,13 @@ int main(){
 	} else {
 		val = atoi(card_name);
 	}
-	printf("The card value is: %i\n", val);
+	/* Check if he value is 3 to 6 */
+	if( (val>2) && (val<7) ){
+		puts("Count has gone up");
+	}
+	/* Otherwise check if the card was 10, J, Q, or K */ 
+	else if( val == 10){
+		puts("Count has gone down");
+	}
 	return 0;
 }
