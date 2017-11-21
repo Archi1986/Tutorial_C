ef115c0ffb908f50929f1246ab88df7d81e1bba7
Yung-Chi_Hsu <Syungchi@gmailcom>
2017-11-21 23:54:06 +0800

    [R] Update code style by switch.

--- card.c
+++ card.c
@@ -3,20 +3,29 @@
 
 int main(){
 	char card_name[3];
-	puts("Enter the card_name: ");
-	scanf("%2s", card_name);
+	//puts("Enter the card_name: ");
+	//scanf("%2s", card_name);
 	int val = 0;
-	if(card_name[0]== 'K'){
-		val = 10;
-	} else if(card_name[0]=='Q'){
-		val = 10;
-	} else if(card_name[0]=='J'){
-		val = 10;
-	} else if(card_name[0]=='A'){
-		val = 11;
-	} else {
-		val = atoi(card_name);
-	}
-	printf("The card value is: %i\n", val);
+
+	do{
+		puts("Enter the card_name: ");
+		scanf("%2s", card_name);
+		val = 0;
+		switch(card_name[0]){
+			case 'K':
+			case 'Q':
+			case 'J':
+				val = 11;
+				break;
+			case 'A':
+				val = 10;
+				break;
+			case 'X':
+				continue;
+			default:
+				val = atoi(card_name);
+		}
+		printf("The card value is: %i\n", val);
+	} while(card_name[0] != 'X');
 	return 0;
 }
